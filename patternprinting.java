382fb8e0b74d2955ac43a51fee8dcaae9072307d
mahamood123-o <shaikmisba305@gmail.com>
2024-12-06 12:41:09 +0530

    Add files via upload

---
+++ patternprinting.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class patternprinting
+{
+    public static void main(String[] args) 
+    {
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.println(j+"");
+            }
+        }
+        System.out.println();
+    }
+}
